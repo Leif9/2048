f8708a47adcaf994d1338776ec5271f6f0356eb0
Leif9 <leifnelsonmelby@gmail.com>
2022-02-25 13:12:26 -0700

    Up and running
    Import all the initial files from my computer

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// Each cell has to be a variable
+
+// w a s d
+
+// Goal 1:
+// Print empty line (16 spaces) with number (2) in a random spot
+// Goal 2:
+// Print 2 or 4 as first number
+// Goal 3:
+// Print grid
+
+int main() { // Pseudo
+    // declare each cell variable using a 2D array
+    // assign each variable null
+
+    // call randomStart  // FIXME put 2 rand nums, not it the same position
+
+    // scan inputs in while (breaks if grid fills or 2048) loop w a s d
+    // if w
+    // shift grid up // FIXME more pseudocode
+    // add same numbers together
+    // if a
+    // if s
+    // if d
+
+}
+
+//func randomStart
+// gen two random number 0 - 3 and 0 - 3
+// if rand == position (eg [2][3]), then arr[2][3] = 2
+// call printNums
+
+//func printNums
+// use for loop with spaces and |
+// 4 x 4
